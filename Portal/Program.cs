828a13602a256e697dd5851b67b9cc6849a863f3
Martijn <martijnschermers2@gmail.com>
2022-10-02 21:14:28 +0200

    Add: Identity and DI

--- Portal/Program.cs
+++ Portal/Program.cs
@@ -1,3 +1,5 @@
+using Core.DomainServices;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SqlServer.Infrastructure;
 
@@ -5,8 +7,30 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<IGameNightRepository, GameNightEFRepository>();
+builder.Services.AddScoped<IGameRepository, GameEFRepository>();
+
 builder.Services.AddDbContext<DomainDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Domain")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Domain"))
+        //TODO: remove in Production 
+        .EnableSensitiveDataLogging());
+
+builder.Services.AddDbContext<IdentityDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Identity"))
+);
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<IdentityDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthorization(options =>
+{
+    //TODO: Add correct policies, example below
+    // options.AddPolicy("OnlyPowerUsersAndUp", policy => policy
+    //     .RequireAuthenticatedUser()
+    //     .RequireClaim("UserType", "poweruser"));
+});
 
 var app = builder.Build();
 
@@ -22,6 +46,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
