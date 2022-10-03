b9d1d5e8907ed39eb147011a970b364288e4e633
Martijn <martijnschermers2@gmail.com>
2022-10-03 17:02:31 +0200

    Finish: Login and Register functionality

--- Portal/Controllers/AccountController.cs
+++ Portal/Controllers/AccountController.cs
@@ -1,41 +1,76 @@
+using System.Security.Claims;
 using Core.Domain;
+using Core.DomainServices;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Portal.Models;
-using SqlServer.Infrastructure;
 
 namespace Portal.Controllers;
 
 public class AccountController : Controller
 {
-    private readonly DomainDbContext _context; 
+    private readonly IUserRepository _repository; 
     private readonly UserManager<IdentityUser> _userManager;
     private readonly SignInManager<IdentityUser> _signInManager;
 
-    public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+    public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IUserRepository repository)
     {
+        _repository = repository;
         _userManager = userManager;
         _signInManager = signInManager;
     }
-    
+
     [HttpGet]
     public IActionResult Login()
     {
+        if (_signInManager.IsSignedIn(new ClaimsPrincipal())) {
+            return RedirectToAction("Index", "Home"); 
+        }
+        
         return View(new LoginViewModel()); 
     }
     
     [HttpPost]
-    public IActionResult Login(LoginViewModel loginViewModel)
+    public async Task<IActionResult> LoginAsync(LoginViewModel loginViewModel)
     {
-        Console.WriteLine(loginViewModel.Email);
-        Console.WriteLine(loginViewModel.Password);
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
         
-        return View(); 
+        var user =  await _userManager.FindByEmailAsync(loginViewModel.Email);
+        if (user == null)
+        {
+            ModelState.AddModelError("", "Invalide emailadres of wachtwoord!");
+            return View();
+        }
+
+        var signinResult = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, true, false);
+
+        if (!signinResult.Succeeded)
+        {
+            ModelState.AddModelError("", "Invalide emailadres of wachtwoord!");
+            return View();
+        }
+
+        return RedirectToAction("Index", "Home");
     }
 
     [HttpGet]
     public IActionResult Register()
     {
+        if (_signInManager.IsSignedIn(new ClaimsPrincipal())) {
+            return RedirectToAction("Index", "Home"); 
+        }
+        
+        var choices = new[] {
+            new { Answer = Gender.Male, Description = "Man" },
+            new { Answer = Gender.Female, Description = "Vrouw" },
+            new { Answer = Gender.Unknown, Description = "Ik zeg het liever niet" }
+        };
+        ViewBag.GenderChoices = new SelectList(choices, "Answer", "Description");
+        
         return View(new RegisterViewModel()); 
     }
     
@@ -48,7 +83,7 @@ public class AccountController : Controller
         
         if (await _userManager.FindByNameAsync(registerViewModel.Username) != null)
         {
-            ModelState.AddModelError("", "User with this username already exists!");
+            ModelState.AddModelError("", "Gebruikersnaam is al in gebruik!");
             return View();
         }
 
@@ -57,11 +92,29 @@ public class AccountController : Controller
             Email = registerViewModel.Email, Gender = registerViewModel.Gender, Name = registerViewModel.Username,
             BirthDate = registerViewModel.BirthDate
         };
+        _repository.AddUser(user);
 
-        var identityUser = new IdentityUser(registerViewModel.Username);
+        var identityUser = new IdentityUser(registerViewModel.Username)
+        {
+            Email = registerViewModel.Email
+        };
         var registerUserResult = await _userManager.CreateAsync(identityUser, registerViewModel.Password);
         
+        if (!registerUserResult.Succeeded)
+        {
+            foreach (var error in registerUserResult.Errors) {
+                ModelState.AddModelError("", error.Description);
+            }
+            return View();
+        }
         
-        return View(); 
+        return RedirectToAction(nameof(Login));
+    }
+
+    public IActionResult Logout()
+    {
+        _signInManager.SignOutAsync();
+        
+        return RedirectToAction("Index", "Home"); 
     }
 }
