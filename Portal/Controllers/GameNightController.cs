365826c1ccf9b1d71cdc402f8d618d64989d8709
Martijn <martijnschermers2@gmail.com>
2022-10-05 14:50:00 +0200

    Finish: organizing gamenight

--- Portal/Controllers/GameNightController.cs
+++ Portal/Controllers/GameNightController.cs
@@ -1,29 +1,40 @@
 using Core.Domain;
 using Core.DomainServices;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Portal.Models;
 
 namespace Portal.Controllers;
 
+[Authorize]
 public class GameNightController : Controller
 {
-    private readonly IGameNightRepository _repository;
+    private readonly IGameNightRepository _gameNightRepository;
+    private readonly IGameRepository _gameRepository;
+    private readonly IUserRepository _userRepository;
 
-    public GameNightController(IGameNightRepository repository)
+    public GameNightController(IGameNightRepository gameNightRepository, IGameRepository gameRepository, IUserRepository userRepository)
     {
-        _repository = repository;
+        _gameNightRepository = gameNightRepository;
+        _gameRepository = gameRepository;
+        _userRepository = userRepository;
     }
 
     public IActionResult Index()
     {
-        var gameNights = _repository.GetAllGameNights();
+        var gameNights = _gameNightRepository.GetAllGameNights();
 
         return View(gameNights);
     }
 
     public IActionResult Organized()
     {
-        return View();
+        var identity = HttpContext.User.Identity;
+        var user = _userRepository.GetUserByEmail(identity.Name);
+        
+        var gameNights = _gameNightRepository.GetAllGameNights().Where(g => g.Organizer == user);
+        
+        return View(gameNights);
     }
 
     public IActionResult Participating()
@@ -34,27 +45,34 @@ public class GameNightController : Controller
     [HttpGet]
     public IActionResult Organize()
     {
-        return View();
+        //TODO: Fix select menu in Organize.html for games
+        // var games = _gameRepository.GetAllGames().Select(g => g.Name);
+        // ViewBag.Games = new SelectList(games, "Name", "Name");
+        
+        return View(new GameNightViewModel());
     }
 
     [HttpPost]
-    public IActionResult Organize(GameNightViewModel gameNight)
+    public IActionResult Organize(GameNightViewModel gameNightViewModel)
     {
-        var convertedGameNight = new GameNight
+        if (!ModelState.IsValid) {
+            return View(); 
+        }
+        
+        var identity = HttpContext.User.Identity;
+        var user = _userRepository.GetUserByEmail(identity.Name);
+
+        var gameNight = new GameNight
         {
             Address = new Address
-                { Street = gameNight.Street, City = gameNight.City, HouseNumber = gameNight.HouseNumber },
+                { Street = gameNightViewModel.Street, City = gameNightViewModel.City, HouseNumber = gameNightViewModel.HouseNumber },
             Drinks = new List<Drink>(), Foods = new List<Food>(), Games = new List<Game>(), Players = new List<User>(),
-            DateTime = gameNight.DateTime, IsPotluck = gameNight.IsPotluck, MaxPlayers = gameNight.MaxPlayers,
-            IsOnlyForAdults = gameNight.IsOnlyForAdults, Organizer = new User{ Email = "martijnschermers2@gmail.com", Gender = Gender.Male, Name = "Martijn", BirthDate = DateTime.Now, Allergies = new List<Allergy>()}
+            DateTime = gameNightViewModel.DateTime, IsPotluck = gameNightViewModel.IsPotluck, MaxPlayers = gameNightViewModel.MaxPlayers,
+            IsOnlyForAdults = gameNightViewModel.IsOnlyForAdults, Organizer = user
         };
         
-        _repository.AddGameNight(convertedGameNight);
+        _gameNightRepository.AddGameNight(gameNight);
 
-        if (ModelState.IsValid) {
-            //Create gamenight
-        }
-
-        return View();
+        return RedirectToAction(nameof(Organized));
     }
 }
--- Portal/Models/GameNightViewModel.cs
+++ Portal/Models/GameNightViewModel.cs
@@ -1,18 +1,24 @@
+using System.ComponentModel.DataAnnotations;
 using Core.Domain;
 
 namespace Portal.Models;
 
 public class GameNightViewModel
 {
-    public User Organizer { get; set; }
+    [Required]
     public DateTime DateTime { get; set; }
+    [Required]
     public int MaxPlayers { get; set; }
+    [Required]
     public bool IsOnlyForAdults { get; set; }
+    [Required]
     public bool IsPotluck { get; set; }
-    
+    [Required]
     public int HouseNumber { get; set; }
+    [Required]
     public string City { get; set; }
+    [Required]
     public string Street { get; set; }
-    
+    [Required]
     public List<Game> Games { get; set; } = new();
 }
