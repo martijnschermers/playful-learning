0279b32b4401b9043cfbd54165c5e80ce302c5a4
Martijn <martijnschermers2@gmail.com>
2022-10-05 20:15:07 +0200

    Add: update gamenight, not working yet

--- Portal/Controllers/GameNightController.cs
+++ Portal/Controllers/GameNightController.cs
@@ -13,7 +13,8 @@ public class GameNightController : Controller
     private readonly IGameRepository _gameRepository;
     private readonly IUserRepository _userRepository;
 
-    public GameNightController(IGameNightRepository gameNightRepository, IGameRepository gameRepository, IUserRepository userRepository)
+    public GameNightController(IGameNightRepository gameNightRepository, IGameRepository gameRepository,
+        IUserRepository userRepository)
     {
         _gameNightRepository = gameNightRepository;
         _gameRepository = gameRepository;
@@ -30,10 +31,10 @@ public class GameNightController : Controller
     public IActionResult Organized()
     {
         var identity = HttpContext.User.Identity;
-        var user = _userRepository.GetUserByEmail(identity.Name);
-        
+        var user = _userRepository.GetUserByEmail(identity!.Name!);
+
         var gameNights = _gameNightRepository.GetAllGameNights().Where(g => g.Organizer == user);
-        
+
         return View(gameNights);
     }
 
@@ -48,7 +49,7 @@ public class GameNightController : Controller
         //TODO: Fix select menu in Organize.html for games
         // var games = _gameRepository.GetAllGames().Select(g => g.Name);
         // ViewBag.Games = new SelectList(games, "Name", "Name");
-        
+
         return View(new GameNightViewModel());
     }
 
@@ -56,21 +57,25 @@ public class GameNightController : Controller
     public IActionResult Organize(GameNightViewModel gameNightViewModel)
     {
         if (!ModelState.IsValid) {
-            return View(); 
+            return View();
         }
-        
+
         var identity = HttpContext.User.Identity;
-        var user = _userRepository.GetUserByEmail(identity.Name);
+        var user = _userRepository.GetUserByEmail(identity!.Name!);
 
         var gameNight = new GameNight
         {
             Address = new Address
-                { Street = gameNightViewModel.Street, City = gameNightViewModel.City, HouseNumber = gameNightViewModel.HouseNumber },
+            {
+                Street = gameNightViewModel.Street, City = gameNightViewModel.City,
+                HouseNumber = gameNightViewModel.HouseNumber
+            },
             Drinks = new List<Drink>(), Foods = new List<Food>(), Games = new List<Game>(), Players = new List<User>(),
-            DateTime = gameNightViewModel.DateTime, IsPotluck = gameNightViewModel.IsPotluck, MaxPlayers = gameNightViewModel.MaxPlayers,
+            DateTime = gameNightViewModel.DateTime, IsPotluck = gameNightViewModel.IsPotluck,
+            MaxPlayers = gameNightViewModel.MaxPlayers,
             IsOnlyForAdults = gameNightViewModel.IsOnlyForAdults, Organizer = user
         };
-        
+
         _gameNightRepository.AddGameNight(gameNight);
 
         return RedirectToAction(nameof(Organized));
@@ -80,8 +85,64 @@ public class GameNightController : Controller
     {
         var id = int.Parse(Url.ActionContext.RouteData.Values["id"]!.ToString()!);
 
-        var gameNight = _gameNightRepository.GetGameNightById(id); 
-        
-        return View(gameNight); 
+        var gameNight = _gameNightRepository.GetGameNightById(id);
+
+        return View(gameNight);
+    }
+
+    [HttpGet]
+    public IActionResult Update()
+    {
+        var id = int.Parse(Url.ActionContext.RouteData.Values["id"]!.ToString()!);
+
+        var gameNight = _gameNightRepository.GetGameNightById(id);
+
+        var viewModel = new GameNightViewModel
+        {
+            City = gameNight.Address.City,
+            Street = gameNight.Address.Street,
+            HouseNumber = gameNight.Address.HouseNumber,
+            Games = gameNight.Games ?? new List<Game>(),
+            DateTime = gameNight.DateTime,
+            IsPotluck = gameNight.IsPotluck,
+            MaxPlayers = gameNight.MaxPlayers,
+            IsOnlyForAdults = gameNight.IsOnlyForAdults
+        };
+
+        return View(viewModel);
+    }
+
+    [HttpPost]
+    public IActionResult Update(GameNightViewModel gameNightViewModel)
+    {
+        var identity = HttpContext.User.Identity;
+        var user = _userRepository.GetUserByEmail(identity!.Name!);
+
+        var id = int.Parse(Url.ActionContext.RouteData.Values["id"]!.ToString()!);
+
+        var updatedGameNight = new GameNight
+        {
+            Id = id,
+            Address = new Address
+            {
+                Street = gameNightViewModel.Street, City = gameNightViewModel.City,
+                HouseNumber = gameNightViewModel.HouseNumber
+            },
+            Games = gameNightViewModel.Games,
+            DateTime = gameNightViewModel.DateTime, IsPotluck = gameNightViewModel.IsPotluck,
+            MaxPlayers = gameNightViewModel.MaxPlayers,
+            IsOnlyForAdults = gameNightViewModel.IsOnlyForAdults, Organizer = user
+        };
+
+        _gameNightRepository.UpdateGameNight(updatedGameNight);
+
+        return RedirectToAction("Organized");
+    }
+
+    public IActionResult Delete()
+    {
+        //TODO: Implement delete function
+
+        return RedirectToAction("Organized");
     }
 }
--- SqlServer.Infrastructure/GameNightEFRepository.cs
+++ SqlServer.Infrastructure/GameNightEFRepository.cs
@@ -6,26 +6,45 @@ namespace SqlServer.Infrastructure;
 
 public class GameNightEFRepository : IGameNightRepository
 {
-    private readonly DomainDbContext _context; 
-    
+    private readonly DomainDbContext _context;
+
     public GameNightEFRepository(DomainDbContext context)
     {
-        _context = context; 
+        _context = context;
     }
-    
+
     public ICollection<GameNight> GetAllGameNights()
     {
-        return _context.GameNights.Include(g => g.Address).Include(g => g.Organizer).ToList(); 
+        return _context.GameNights
+            .Include(g => g.Address)
+            .Include(g => g.Organizer)
+            .Include(g => g.Players)
+            .ToList();
     }
 
     public void AddGameNight(GameNight gameNight)
     {
         _context.GameNights.Add(gameNight);
-        _context.SaveChanges(); 
+        _context.SaveChanges();
     }
 
     public GameNight GetGameNightById(int id)
     {
-        return _context.GameNights.First(g => g.Id == id); 
+        return _context.GameNights
+            .Where(g => g.Id == id)
+            .Include(g => g.Address)
+            .Include(g => g.Organizer)
+            .Include(g => g.Players)
+            .First();
+    }
+
+    public async void UpdateGameNight(GameNight originalGameNight)
+    {
+        _context.Entry(await _context.GameNights
+                .FirstOrDefaultAsync(x => x.Id == originalGameNight.Id))
+                .CurrentValues
+                .SetValues(originalGameNight);
+
+        await _context.SaveChangesAsync(); 
     }
 }
