9e108a3136763981c9d6798d7e5e1bb3769f6c96
Martijn <martijnschermers2@gmail.com>
2022-10-06 19:31:56 +0200

    Add: CRUD methods gamenight

--- Core.DomainServices/IGameNightRepository.cs
+++ Core.DomainServices/IGameNightRepository.cs
@@ -5,7 +5,10 @@ namespace Core.DomainServices;
 public interface IGameNightRepository
 {
     ICollection<GameNight> GetAllGameNights();
+    ICollection<GameNight> GetParticipating(User user);
     void AddGameNight(GameNight gameNight);
     GameNight GetGameNightById(int id);
-    void UpdateGameNight(GameNight updatedGameNight); 
+    void UpdateGameNight(GameNight updatedGameNight);
+    void DeleteGameNight(int gameNightId);
+    void Participate(int gameNightId, User user); 
 }
