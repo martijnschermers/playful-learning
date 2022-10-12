164bde65b3e4adb0211d03d8aece3515bb43e941
martijnschermers <martijnschermers2@gmail.com>
2022-10-12 21:39:56 +0200

    Add: unittest projects

---
+++ Portal.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace Portal.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ SqlServer.Infrastructure.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace SqlServer.Infrastructure.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
