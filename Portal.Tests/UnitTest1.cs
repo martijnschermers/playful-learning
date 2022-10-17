2b220877cf7ea612f55aaf80943625986bf38229
Martijn <martijnschermers2@gmail.com>
2022-10-17 13:50:51 +0200

    Add: Basic portal test, not working yet

--- Portal.Tests/UnitTest1.cs
+++ Portal.Tests/UnitTest1.cs
@@ -1,11 +1,28 @@
+using Core.DomainServices;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Portal.Controllers;
 using Xunit;
 
 namespace Portal.Tests;
 
 public class UnitTest1
 {
-    [Fact]
+    // [Fact]
     public void Test1()
     {
+        // Arrange
+        var repositoryMock = new Mock<IGameNightRepository>();
+        var gameRepoMock = new Mock<IGameRepository>(); 
+        var userRepoMock = new Mock<IUserRepository>(); 
+        var gameNightServiceMock = new Mock<IGameNightService>();
+
+        var controller = new GameNightController(repositoryMock.Object, gameRepoMock.Object, userRepoMock.Object, gameNightServiceMock.Object);
+        
+        // Act
+        var result = controller.Organized() as ViewResult;
+
+        // Assert
+        Assert.Null(result.ViewName);
     }
 }
