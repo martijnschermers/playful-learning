53ca613d5fe0943170236f663702070964d61fc4
Martijn <martijnschermers2@gmail.com>
2022-10-21 11:50:41 +0200

    Add: drinks table with seed data

--- SqlServer.Infrastructure/DomainDbContext.cs
+++ SqlServer.Infrastructure/DomainDbContext.cs
@@ -4,36 +4,61 @@ using Microsoft.EntityFrameworkCore;
 namespace SqlServer.Infrastructure;
 
 public class DomainDbContext : DbContext
-{ 
-    public DomainDbContext(DbContextOptions<DomainDbContext> options) : base(options) { }
-    
+{
+    public DomainDbContext(DbContextOptions<DomainDbContext> options) : base(options)
+    {
+    }
+
     public DbSet<GameNight> GameNights { get; set; } = null!;
     public DbSet<Game> Games { get; set; } = null!;
     public DbSet<User> Users { get; set; } = null!;
     public DbSet<Allergy> Allergies { get; set; } = null!;
-    
+    public DbSet<Drink> Drinks { get; set; } = null!;
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         var games = new List<Game>
         {
-            new Game { Id = 1, Name = "Uno", Description = "Versimpelde versie van pesten!", Genre = Genre.Kids, Image = "", Type = GameType.Card, IsOnlyForAdults = false }, 
-            new Game { Id = 2, Name = "Ticket to Ride", Description = "Het spel met treinen!", Genre = Genre.Kids, Image = "", Type = GameType.Board, IsOnlyForAdults = false }, 
-            new Game { Id = 3, Name = "Monopoly", Description = "Het spel met geld.", Genre = Genre.Kids, Image = "", Type = GameType.Board, IsOnlyForAdults = false }
+            new Game
+            {
+                Id = 1, Name = "Uno", Description = "Versimpelde versie van pesten!", Genre = Genre.Kids, Image = "",
+                Type = GameType.Card, IsOnlyForAdults = false
+            },
+            new Game
+            {
+                Id = 2, Name = "Ticket to Ride", Description = "Het spel met treinen!", Genre = Genre.Kids, Image = "",
+                Type = GameType.Board, IsOnlyForAdults = false
+            },
+            new Game
+            {
+                Id = 3, Name = "Monopoly", Description = "Het spel met geld.", Genre = Genre.Kids, Image = "",
+                Type = GameType.Board, IsOnlyForAdults = false
+            }
         };
 
         var allergies = new List<Allergy>
         {
-            new Allergy { Id = 1, Name = AllergyEnum.Gluten, Description = "Gluten"},
-            new Allergy { Id = 2, Name = AllergyEnum.Lactose, Description = "Lactose"},
-            new Allergy { Id = 3, Name = AllergyEnum.Nuts, Description = "Noten"},
+            new Allergy { Id = 1, Name = AllergyEnum.Gluten, Description = "Gluten" },
+            new Allergy { Id = 2, Name = AllergyEnum.Lactose, Description = "Lactose" },
+            new Allergy { Id = 3, Name = AllergyEnum.Nuts, Description = "Noten" },
             new Allergy { Id = 4, Name = AllergyEnum.Soya, Description = "Soja" },
-            new Allergy { Id = 5, Name = AllergyEnum.Wheat, Description = "Tarwe"},
-            new Allergy { Id = 6, Name = AllergyEnum.Vegan, Description = "Vegetarisch"}
+            new Allergy { Id = 5, Name = AllergyEnum.Wheat, Description = "Tarwe" },
+            new Allergy { Id = 6, Name = AllergyEnum.Vegan, Description = "Vegetarisch" }
+        };
+
+        var drinks = new List<Drink>
+        {
+            new Drink { Id = 1, Name = "Bier", ContainsAlcohol = true },
+            new Drink { Id = 2, Name = "Water", ContainsAlcohol = false },
+            new Drink { Id = 3, Name = "Cola", ContainsAlcohol = false },
+            new Drink { Id = 4, Name = "Wijn", ContainsAlcohol = true },
+            new Drink { Id = 5, Name = "Fanta", ContainsAlcohol = false }
         };
 
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.Entity<Game>().HasData(games);
         modelBuilder.Entity<Allergy>().HasData(allergies);
+        modelBuilder.Entity<Drink>().HasData(drinks);
     }
 }
