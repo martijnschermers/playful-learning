c9d6655e49ac376591283f523378160b543c6c1c
Martijn <martijnschermers2@gmail.com>
2022-10-24 16:01:23 +0200

    Update: made GameId nullable

--- Core.Domain/Game.cs
+++ Core.Domain/Game.cs
@@ -2,7 +2,7 @@ namespace Core.Domain;
 
 public class Game
 {
-    public int Id { get; set; }
+    public int? Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public bool IsOnlyForAdults { get; set; }
